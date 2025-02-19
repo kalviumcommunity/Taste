1ee7733ad4ad13ad36b7d525c2e07e195a221f71
kalviumcommunity <sibishree2002@gmail.com>
2025-02-19 12:06:37 +0530

    completed crud operations

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const { connectDatabase, getDatabaseStatus } = require("./Database");
+const routes = require('./routes'); 
+app.use(express.json());
+
+app.use(routes);
+
+// app.use(routes) => is like saying, "Hey server, whenever a request comes in, check the routes I defined in routes.js to decide what to do!"
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: ".env" });
@@ -9,7 +15,6 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 connectDatabase();
 
-app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Connected the server to the Database.")
